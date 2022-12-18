240176ecd671daaa5291faf0fab5ea3aacb975b6
Abdelbaki Boukerche <boukerche.abdelbaki@gmail.com>
2022-12-18 06:55:52 +0100

    first commit

---
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -0,0 +1,30 @@
+import { Component, AfterContentInit, ContentChildren, QueryList } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
+
+@Component({
+  selector: 'app-tabs-container',
+  templateUrl: './tabs-container.component.html',
+  styleUrls: ['./tabs-container.component.css']
+})
+export class TabsContainerComponent implements AfterContentInit {
+
+  @ContentChildren(TabComponent) tabs?: QueryList<TabComponent>
+
+  ngAfterContentInit(): void {
+    const activeTabs = this.tabs?.filter(tab => tab.active === true)
+
+    if (!activeTabs || activeTabs.length === 0) {
+      this.selectTab(this.tabs!.first)
+    }
+  }
+
+  selectTab(tab: TabComponent): boolean {
+    this.tabs?.forEach(tab => {
+      tab.active = false
+    })
+    tab.active = true
+
+    return false
+  }
+
+}
---
+++ src/app/user/auth-modal/auth-modal.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ModalService } from 'src/app/services/modal.service';
+
+@Component({
+  selector: 'app-auth-modal',
+  templateUrl: './auth-modal.component.html',
+  styleUrls: ['./auth-modal.component.css']
+})
+export class AuthModalComponent implements OnInit, OnDestroy {
+  constructor(public modal: ModalService) { }
+
+  private modalID = 'auth'
+
+  ngOnInit(): void {
+    this.modal.register(this.modalID)
+  }
+
+  ngOnDestroy(): void {
+    this.modal.unregister(this.modalID)
+  }
+}
